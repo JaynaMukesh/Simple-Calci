bc9e80688d19a3a50a921e282526c46198bda982
JaynaMukesh <jaynamukee@gmail.com>
2022-11-14 13:42:55 +0530

    iiiiiiiiiiiiiiiiiiiiiiii

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void) {
+  float val1, val2, answer;
+  char operator;
+
+  printf("Enter the calculation: ");
+  scanf("%f %c %f", &val1, &operator, & val2);
+
+  switch (operator) {
+  case '+':
+    answer = val1 + val2;
+    break;
+  case '-':
+    answer = val1 - val2;
+    break;
+  case '/':
+    answer = val1 / val2;
+    break;
+  case '*':
+    answer = val1 * val2;
+    break;
+    default:
+    printf("Error!");
+  }
+  printf("Answer: %g", answer);
+  return 0;
+}
